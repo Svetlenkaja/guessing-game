ba1aa12093df05fd9e3c40143f67b1eaf162d6eb
Svetlenkaja <Svetlenkajas@gmail.com>
2024-10-25 21:35:56 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.candidate = Math.ceil((this.min + this.max) / 2);
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
